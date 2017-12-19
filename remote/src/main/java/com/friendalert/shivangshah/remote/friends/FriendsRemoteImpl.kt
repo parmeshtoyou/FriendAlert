dd2d16df56bf6df8b8bbacd4d8dc8b7729cbd8ba
Shivang Shah <shivangshah@Shivangs-MBP.home>
2017-12-19 00:58:45 -0500

    reformat - small change

--- remote/src/main/java/com/friendalert/shivangshah/remote/friends/FriendsRemoteImpl.kt
+++ remote/src/main/java/com/friendalert/shivangshah/remote/friends/FriendsRemoteImpl.kt
@@ -15,23 +15,20 @@ class FriendsRemoteImpl  @Inject constructor(private val friendsService: Friends
 
     override fun getFriends(userId: String, contacts: ContactsRequestModel): Single<FriendsResponseModel> {
 
-        val contacts2 = ArrayList<String>()
-        contacts2.add("123")
-        contacts2.add("2342423")
-        contacts2.add("111111")
-        contacts2.add("928340293")
+        return friendsService.getFriends(userId, contacts)
 
-        return friendsService.getFriends("10155348650147990", ContactsRequestModel(contacts2))
-
-        //return friendsService.getFriends(userId, contacts)
     }
 
     override fun createFriendRequest(senderId: String, receiverId: String): Single<CreateFriendRequestResponseModel> {
+
         return friendsService.createFriendRequest(senderId, receiverId)
+
     }
 
     override fun updateFriend(id: String, status: Int): Single<UpdateFriendResponseModel> {
+
         return friendsService.updateFriend(id, status)
+        
     }
 
 }
